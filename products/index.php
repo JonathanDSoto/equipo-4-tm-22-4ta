351b4aa7d8bc32877b035d62a783c45ad6d5ce57
Manuel Alejandro Diaz Hernandez <Manuel_alejandro090401@outlook.com>
2022-10-14 21:24:51 -0600

    update

--- products/index.php
+++ products/index.php
@@ -31,98 +31,33 @@
         <div class="main-content">
             <?php include '../layouts/bread.templete.php'?>
 
-            <!-- End Page-content -->
-            <div class="row"> 
+            <div class="mt-1 ms-5 me-5 mb-5 ">
+                <div class="row row-cols-1 row-cols-md-5 g-4">
 
-                <div class="col-xl-12 col-lg-12">
-                    <div>
-                        <div class="card">
-                            <div class="card-header border-0">
-                                <div class="row g-4">
-                                    <div class="col-sm-auto">
-                                        <div>
-                                            <a href="apps-ecommerce-add-product.html" class="btn btn-success" id="addproduct-btn"><i class="ri-add-line align-bottom me-1"></i> Add Product</a>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm">
-                                        <div class="d-flex justify-content-sm-end">
-                                            <div class="search-box ms-2">
-                                                <input type="text" class="form-control" id="searchProductList" placeholder="Search Products...">
-                                                <i class="ri-search-line search-icon"></i>
-                                            </div>
-                                        </div>
+
+                    <!-- CARTA INICIO -->
+                    
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="<?=BASE_PATH?>/public/images/mac-img.png" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center">Nombre producto</h5>
+                                    <p class="card-text text-truncate d-block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet suscipit beatae totam facere eligendi similique sequi ratione ipsum hic cupiditate.</p><a href="#" id="">Ver más</a>
+                                    <hr>
+                                    <p class="text-end me-4  text-dark">$999</p>
+                                    <div class="row">
+                                        <button type="button" class="m-auto btn btn-info col-6">Ver más</button>
+                                        <button type="button" class="m-auto btn btn-danger col-4">Añadir <i class="mdi mdi-cart"></i> </button>
                                     </div>
                                 </div>
                             </div>
-
-                            <div class="card-header">
-                                <div class="row align-items-center">
-                                    <div class="col">
-                                        <ul class="nav nav-tabs-custom card-header-tabs border-bottom-0" role="tablist">
-                                            <li class="nav-item">
-                                                <a class="nav-link active fw-semibold" data-bs-toggle="tab" href="#productnav-all" role="tab">
-                                                    All <span class="badge badge-soft-danger align-middle rounded-pill ms-1">12</span>
-                                                </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link fw-semibold" data-bs-toggle="tab" href="#productnav-published" role="tab">
-                                                    Published <span class="badge badge-soft-danger align-middle rounded-pill ms-1">5</span>
-                                                </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link fw-semibold" data-bs-toggle="tab" href="#productnav-draft" role="tab">
-                                                    Draft
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-auto">
-                                        <div id="selection-element">
-                                            <div class="my-n1 d-flex align-items-center text-muted">
-                                                Select <div id="select-content" class="text-body fw-semibold px-1"></div> Result <button type="button" class="btn btn-link link-danger p-0 ms-3 shadow-none" data-bs-toggle="modal" data-bs-target="#removeItemModal">Remove</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- end card header -->
-                            <div class="card-body">
-
-                                <div class="tab-content text-muted">
-                                    <div class="tab-pane active" id="productnav-all" role="tabpanel">
-                                        <div id="table-product-list-all" class="table-card gridjs-border-none"></div>
-                                    </div>
-                                    <!-- end tab pane -->
-
-                                    <div class="tab-pane" id="productnav-published" role="tabpanel">
-                                        <div id="table-product-list-published" class="table-card gridjs-border-none"></div>
-                                    </div>
-                                    <!-- end tab pane -->
-
-                                    <div class="tab-pane" id="productnav-draft" role="tabpanel">
-                                        <div class="py-4 text-center">
-                                            <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop" colors="primary:#405189,secondary:#0ab39c" style="width:72px;height:72px">
-                                            </lord-icon>
-                                            <h5 class="mt-4">Sorry! No Result Found</h5>
-                                        </div>
-                                    </div>
-                                    <!-- end tab pane -->
-                                </div>
-                                <!-- end tab content -->
-
-                            </div>
-                            <!-- end card body -->
-                        </div>
-                        <!-- end card -->
                     </div>
+
+
+
+
                 </div>
-                <!-- end col -->
             </div>
-            <!-- end row -->
-
-            <!-- End Page-content -->
-
-
 
             <?php include "../layouts/footer.template.php"; ?>
         </div>
@@ -133,11 +68,6 @@
 
 
 
-    <!--start back-to-top-->
-    <button onclick="topFunction()" class="btn btn-danger btn-icon" id="back-to-top">
-        <i class="ri-arrow-up-line"></i>
-    </button>
-    <!--end back-to-top-->
 
     <!--preloader-->
     <div id="preloader">
