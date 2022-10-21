cc3d0651b6e64cac83f680334303a85b5a1bfa7a
IsaelCota <isaelkeki107@hotmail.com>
2022-10-21 15:48:09 -0600

    Usuarios en proceso

--- user/users.php
+++ user/users.php
@@ -88,15 +88,15 @@
                                     <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="editUser(user)">
                                         <i class="mdi mdi-pencil"></i>
                                     </button>
-                                    <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editPhotoModal">
+                                    <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editPhotoModal" @click="editarFotoUser(user)">
                                         <i class="mdi mdi-camera-flip"></i>
                                     </button>
                                     
                                     <form method="POST" action="<?= BASE_PATH ?>Controlador-usuarios">
-                                        <button type="submit" class="btn btn-danger">
+                                        <button type="submit" class="btn btn-danger" @click="alertaEliminar">
                                             <i class="mdi mdi-trash-can-outline"></i>
                                             <input type="hidden" name="action" action="eliminarUsuario" value="eliminarUsuario">
-                                            <input type="hidden" name="id" :value="">
+                                            <input type="hidden" name="id" :value="user.id">
 
                                             <input type="hidden" name="global_token" value="<?= $_SESSION['global_token'] ?>">
                                         </button>
@@ -109,89 +109,93 @@
             <!-- end table -->
             </div>
             <!-- Modal de crear -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">{{titulo_modal}}</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form enctype="multipart/form-data" method="POST" action="<?= BASE_PATH ?>Controlador-usuarios" >
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">Nombre</span>
-                            <input type="text" class="form-control" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1" name="name">
-                        </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">Apellidos</span>
-                            <input type="text" class="form-control" placeholder="Apellidos" aria-label="Username" aria-describedby="basic-addon1" name="lastname">
-                        </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">Email</span>
-                            <input type="text" class="form-control" placeholder="example@example.com" aria-label="Username" aria-describedby="basic-addon1" name="email">
-                        </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">Numero</span>
-                            <input type="text" class="form-control" placeholder="Numero" aria-label="Username" aria-describedby="basic-addon1" name="phone_number">
-                        </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">Creado por:</span>
-                            <input type="text" class="form-control" placeholder="Creado por" aria-label="Username" aria-describedby="basic-addon1" name="created_by">
-                        </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">Rol</span>
-                            <input type="text" class="form-control" placeholder="Rol" aria-label="Username" aria-describedby="basic-addon1" name="role">
-                        </div>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">Contraseña</span>
-                            <input type="password" class="form-control" placeholder="******" aria-label="Username" aria-describedby="basic-addon1" name="password">
-                        </div>
-                        <div class="input-group mb-3">
-                            <input type="file" class="form-control" name="profile_photo_file" aria-label="Username" aria-describedby="basic-addon1">
-                        </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">{{titulo_modal}}</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form enctype="multipart/form-data" method="POST" action="<?= BASE_PATH ?>Controlador-usuarios" >
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">Nombre</span>
+                                <input type="text" class="form-control" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1" name="name" v-if="mostrar_inputs_añadir == true">
+                                <input type="text" class="form-control" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1" v-if="mostrar_inputs_editar == true" name="name" :value="datos_user.name">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">Apellidos</span>
+                                <input type="text" class="form-control" placeholder="Apellidos" aria-label="Username" aria-describedby="basic-addon1" name="lastname" v-if="mostrar_inputs_añadir == true">
+                                <input type="text" class="form-control" placeholder="Apellidos" aria-label="Username" aria-describedby="basic-addon1" name="lastname" v-if="mostrar_inputs_editar == true" :value="datos_user.lastname">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">Email</span>
+                                <input type="text" class="form-control" placeholder="example@example.com" aria-label="Username" aria-describedby="basic-addon1" name="email" v-if="mostrar_inputs_añadir == true">
+                                <input type="text" class="form-control" placeholder="example@example.com" aria-label="Username" aria-describedby="basic-addon1" name="email" v-if="mostrar_inputs_editar == true" :value="datos_user.email">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">Numero</span>
+                                <input type="text" class="form-control" placeholder="Numero" aria-label="Username" aria-describedby="basic-addon1" name="phone_number" v-if="mostrar_inputs_añadir == true">
+                                <input type="text" class="form-control" placeholder="Numero" aria-label="Username" aria-describedby="basic-addon1" name="phone_number" v-if="mostrar_inputs_editar == true" :value="datos_user.phone_number">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">Creado por:</span>
+                                <input type="text" class="form-control" placeholder="Creado por" aria-label="Username" aria-describedby="basic-addon1" name="created_by" v-if="mostrar_inputs_añadir == true">
+                                <input type="text" class="form-control" placeholder="Creado por" aria-label="Username" aria-describedby="basic-addon1" name="created_by" v-if="mostrar_inputs_editar == true" :value="datos_user.created_by">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">Rol</span>
+                                <input type="text" class="form-control" placeholder="Rol" aria-label="Username" aria-describedby="basic-addon1" name="role" v-if="mostrar_inputs_añadir == true">
+                                <input type="text" class="form-control" placeholder="Rol" aria-label="Username" aria-describedby="basic-addon1" name="role" v-if="mostrar_inputs_editar == true" :value="datos_user.role">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">Contraseña</span>
+                                <input type="password" class="form-control" placeholder="******" aria-label="Username" aria-describedby="basic-addon1" name="password">
+                            </div>
+                            <div class="input-group mb-3" v-if="mostrar_añadir_foto == true">
+                                <input type="file" class="form-control" name="profile_photo_file" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
 
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                            <button type="submit" class="btn btn-success">Guardar</button>
-                            <input type="hidden" name="action" action="nuevoUsuario" value="nuevoUsuario">
-                            <input type="hidden" name="global_token" value="<?= $_SESSION['global_token'] ?>">
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                                <button type="submit" class="btn btn-success" @click="alertaEditar">Guardar {{btn_guardar_cambios}}</button>
+                                <input type="hidden" name="action" :action="accion" :value="accion">
+                                <input type="hidden" name="global_token" value="<?= $_SESSION['global_token'] ?>">
+                                <input type="hidden" name="id" :value="datos_user.id">
 
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Modal editar foto -->
-    <div class="modal fade" id="editPhotoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Editar foto</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form enctype="multipart/form-data" method="POST" action="<?= BASE_PATH ?>Controlador-usuarios" >
-                        <div class="input-group mb-3">
-                            <input type="file" class="form-control" name="profile_photo_file" aria-label="Username" aria-describedby="basic-addon1">
-                        </div>
-
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                            <button type="submit" class="btn btn-success">Guardar</button>
-                            <?php 
-                            echo $ID_user_editar_foto;
-                            ?>
-                            <input type="hidden" name="action" action="editarFotoPerfil" value="editarFotoPerfil">
-                            <input type="hidden" name="id" value="<?= $user->id; ?>">
-                            <input type="hidden" name="global_token" value="<?= $_SESSION['global_token'] ?>">
-                        </div>
-                    </form>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+
+        <!-- Modal editar foto -->
+        <div class="modal fade" id="editPhotoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Editar foto</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form enctype="multipart/form-data" method="POST" action="<?= BASE_PATH ?>Controlador-usuarios" >
+                            <div class="input-group mb-3">
+                                <input type="file" class="form-control" name="profile_photo_file" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                                <button type="submit" class="btn btn-success">Guardar</button>
+                                <input type="hidden" name="action" action="editarFotoPerfil" value="editarFotoPerfil">
+                                <input type="hidden" name="id" :value="id_user.id">
+                                <input type="hidden" name="global_token" value="<?= $_SESSION['global_token'] ?>">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <!-- end table responsive -->
         
@@ -216,8 +220,11 @@
     <!-- profile init js -->
     <script src="assets/js/pages/profile.init.js"></script>
 
-            <!-- Vue js -->
-            <script src="https://unpkg.com/vue@3"></script>
+    <!-- Vue js -->
+    <script src="https://unpkg.com/vue@3"></script>
+
+    <!--SweetAlert-->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!--script vue -->
     <script>
@@ -227,21 +234,55 @@
                     return {
 
                         users: <?php echo json_encode($data);?>,
-                        datos_user: {nombre: "", apellido: "", correo: "", numero: "", creado: "", rol: ""},
+                        datos_user: {id: "",nombre: "", apellido: "", correo: "", numero: "", creado: "", rol: ""},
                         titulo_modal: "",
-                        
+                        accion: "",
+                        mostrar_añadir_foto: true,
+                        id_user: {id: ""},
+                        mostrar_inputs_editar: true,
+                        mostrar_inputs_añadir: true,
+                        btn_guardar_cambios: "",
                     }
                 },
                 methods:{
                     editUser(user){
                         this.titulo_modal = "Editar usuario";
                         this.datos_user = user;
-                        
+                        this.accion = "editarUsuario";
+                        this.mostrar_añadir_foto = false;
+                        this.btn_guardar_cambios = "Cambios";
+                        this.mostrar_inputs_editar = true;
+                        this.mostrar_inputs_añadir = false;
                     },
                     agregarUser(){
                         this.titulo_modal = "Agregar usuario";
+                        this.accion = "nuevoUsuario";   
+                        this.mostrar_añadir_foto = true;
+                        this.btn_guardar_cambios = "";
+                        this.mostrar_inputs_editar = false;
+                        this.mostrar_inputs_añadir = true;
                     },
-                    
+                    editarFotoUser(user){
+                        this.id_user = user;
+                    },
+                    alertaEditar(){
+                        Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: 'Cambios Guardados',
+                        showConfirmButton: false,
+                        timer: 10000
+                        })
+                    },
+                    alertaEliminar(){
+                        Swal.fire({
+                        position: 'center',
+                        icon: 'info',
+                        title: 'Usuario Eliminado',
+                        showConfirmButton: false,
+                        timer: 15000
+                        })
+                    },
                 },
                 mounted() {
 
