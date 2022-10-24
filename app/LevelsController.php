2d7aaa10600142811f227e999d4afaac35c6e248
Ivan Martin Aviles Moreira <ivanaviles569@gmail.com>
2022-10-23 21:17:49 -0600

    Actualizado LevelsController

--- app/LevelsController.php
+++ app/LevelsController.php
@@ -1,9 +1,46 @@
 <?php
 include_once "config.php";
+if (isset($_POST['action'])) {
 
+	if ( isset($_POST['global_token']) && 
+		$_POST['global_token'] == $_SESSION['global_token']) {
+
+		switch ($_POST['action']) {
+            
+			case 'nuevaLevel':
+				
+				$levelsController = new LevelsController();
+
+                $name = strip_tags($_POST['name']);
+				$percentage_discount = strip_tags($_POST['percentage_discount']);
+
+				$levelsController->newLevel($name,$percentage_discount);
+
+			break;
+            case 'editarLevel':
+                $levelsController = new LevelsController();
+
+				$id = strip_tags($_POST['id']);
+                $name = strip_tags($_POST['name']);
+				$percentage_discount = strip_tags($_POST['percentage_discount']);
+                
+
+				$levelsController->editLevel($id,$name,$percentage_discount);
+            break;
+            case 'eliminarLevel':
+                $levelsController = new LevelsController();
+
+                $id = strip_tags($_POST['id']);
+
+				$levelsController->deleteLevel($id);
+            break;
+		}
+
+	}
+}
 Class LevelsController
 {
-
+//Todos los levels
 	public function getLevels()
 	{
 		$curl = curl_init();
@@ -34,6 +71,7 @@ Class LevelsController
 			return array();
 		}
 	}
+//Busqueda de level por su id Levels
     public function idLevel($id)
 	{
 		$curl = curl_init();
@@ -64,6 +102,106 @@ Class LevelsController
 			return array();
 		}
 	}
+//Nuevo Level
+public function newLevel($name,$percentage_discount)
+{
+
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://crud.jonathansoto.mx/api/levels',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_POSTFIELDS => array('name' => $name,'percentage_discount' => $percentage_discount),
+		CURLOPT_HTTPHEADER => array(
+			'Authorization: Bearer '.$_SESSION['token']
+		),
+	  ));
+
+	$response = curl_exec($curl); 
+	curl_close($curl);
+	$response = json_decode($response);
+
+	if ( isset($response->code) && $response->code > 0) {
+
+		header("Location:".BASE_PATH."productos");
+	}else{
+		#var_dump($response);
+		header("Location:".BASE_PATH."?error=true");
+	}
+
+}
+
+//Editar Level
+public function editLevel($id,$name,$percentage_discount)
+{
+
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://crud.jonathansoto.mx/api/levels',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'PUT',
+		CURLOPT_POSTFIELDS => 'id='.$id.'&name='.$name.'&percentage_discount='.$percentage_discount,
+		CURLOPT_HTTPHEADER => array(
+			'Authorization: Bearer '.$_SESSION['token'],
+		  'Content-Type: application/x-www-form-urlencoded'
+		),
+	  ));
+
+	$response = curl_exec($curl); 
+	curl_close($curl);
+	$response = json_decode($response);
+
+	if ( isset($response->code) && $response->code > 0) {
+
+		header("Location:".BASE_PATH."productos");
+	}else{
+		#var_dump($response);
+		header("Location:".BASE_PATH."?error=true");
+	}
+
+}
+//Eliminar Level
+public function deleteBrand($id)
+{
+
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://crud.jonathansoto.mx/api/levels/'.$id,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'DELETE',
+		CURLOPT_HTTPHEADER => array(
+			'Authorization: Bearer '.$_SESSION['token']
+		),
+	  ));
+
+	$response = curl_exec($curl); 
+	curl_close($curl);
+	$response = json_decode($response);
+
+	if ( isset($response->code) && $response->code > 0) {
+
+		header("Location:".BASE_PATH."productos");
+	}else{
+		#var_dump($response);
+		header("Location:".BASE_PATH."?error=true");
+	}
+
+}
 
 }
 
