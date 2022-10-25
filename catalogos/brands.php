d8f2b3dbd043542c18cb4e951c1baee1236d946a
Isael742 <isaelkeki107@hotmail.com>
2022-10-24 22:52:27 -0600

    Crud brands implementado

--- catalogos/brands.php
+++ catalogos/brands.php
@@ -1,5 +1,8 @@
 <?php 
     include_once "../app/config.php";
+    include '..\app\BrandController.php';
+    $brands = new BrandController();
+    $data = $brands->getBrands();
 ?> 
 
 <!doctype html>
@@ -31,7 +34,7 @@
     <!-- ============================================================== -->
     <!-- Start right Content here -->
     <!-- ============================================================== -->
-    <div class="main-content" >
+    <div class="main-content" id="app">
         <div class="page-content mb-n5">
             <div class="container-fluid">
                 <!-- start page title -->
@@ -47,7 +50,7 @@
             <!-- container-fluid -->
         </div>
             
-            <button class="btn btn-success ms-5" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+            <button class="btn btn-success ms-5" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="agregarBrand">
                 Crear brand
                 <i class="mdi mdi-plus-thick"></i>
             </button>
@@ -64,32 +67,29 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        <tr v-for="(brand, index) in brands">
                 
-                            <td><a href="#" class="fw-semibold">{{id}}</a></td>
+                            <td><a href="#" class="fw-semibold">{{brand.id}}</a></td>
                             <td>                            
                                 <div class="d-flex gap-2 align-items-center">
-                                    <div class="flex-shrink-0">
-                                        <img src="" alt="avatar" class="avatar-xs rounded-circle" />
-                                    </div>
                                     <div class="flex-grow-1">
-                                    {{name}}
+                                    {{brand.name}}
                                     </div>
                                 </div>
                             </td>
-                            <td>{{decripción}}</td>
+                            <td>{{brand.description}}</td>
                             <!-- <td class="text-danger"><i class="ri-close-circle-line fs-17 align-middle">Cancel</i></td> -->
-                            <td>{{slug}}</td>
+                            <td>{{brand.slug}}</td>
                             <td class="text-center row">
                                 <div class="col row-cols-12">
-                                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+                                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="editBrand(brand)">
                                         <i class="mdi mdi-pencil"></i>
                                     </button>
-                                    <form method="POST" action="">
-                                        <button type="submit" class="btn btn-danger">
+                                    <form method="POST" action="<?= BASE_PATH ?>Controlador-brand">
+                                        <button type="submit" class="btn btn-danger" @click="alertaEliminar">
                                             <i class="mdi mdi-trash-can-outline"></i>
-                                            <input type="hidden" name="action" action="">
-                                            <input type="hidden" name="id" value="">
+                                            <input type="hidden" name="action" action="eliminarBrand" value="eliminarBrand">
+                                            <input type="hidden" name="id" :value="brand.id">
                                             <input type="hidden" name="global_token" value="<?= $_SESSION['global_token'] ?>">
                                         </button>
                                     </form>
@@ -109,28 +109,32 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <form enctype="multipart/form-data" method="POST" action="" >
+                        <form method="POST" action="<?= BASE_PATH ?>Controlador-brand">
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Nombre</span>
-                                <input type="text" class="form-control" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1" name="name" >
+                                <input type="text" class="form-control" placeholder="Nombre de Brand" aria-label="Username" aria-describedby="basic-addon1" v-if="mostrar_inputs_añadir == true" name="name" >
+                                <input type="text" class="form-control" placeholder="Nombre de Brand" aria-label="Username" aria-describedby="basic-addon1" v-if="mostrar_inputs_editar == true" name="name" :value="datos_brand.name">
+                                
                                 
                             </div>
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Descripción</span>
-                                <input type="text" class="form-control" placeholder="example@example.com" aria-label="Username" aria-describedby="basic-addon1" name="email">
+                                <input type="text" class="form-control" placeholder="Descripción" aria-label="Username" aria-describedby="basic-addon1" v-if="mostrar_inputs_añadir == true" name="description">
+                                <input type="text" class="form-control" placeholder="Descripción" aria-label="Username" aria-describedby="basic-addon1" v-if="mostrar_inputs_editar == true" name="description" :value="datos_brand.description">
                                 
                             </div>
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Slug</span>
-                                <input type="text" class="form-control" placeholder="Numero" aria-label="Username" aria-describedby="basic-addon1" name="phone_number" >
+                                <input type="text" class="form-control" placeholder="Slug" aria-label="Username" aria-describedby="basic-addon1" v-if="mostrar_inputs_añadir == true" name="slug">
+                                <input type="text" class="form-control" placeholder="Slug" aria-label="Username" aria-describedby="basic-addon1" v-if="mostrar_inputs_editar == true" name="slug" :value="datos_brand.slug">
                             
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                                <button type="submit" class="btn btn-success">Guardar</button>
+                                <button type="submit" class="btn btn-success" @click="alertaEditar">Guardar {{btn_guardar_cambios}}</button>
                                 <input type="hidden" name="action" :action="accion" :value="accion">
                                 <input type="hidden" name="global_token" value="<?= $_SESSION['global_token'] ?>">
-                                <input type="hidden" name="id" :value="datos_user.id">
+                                <input type="hidden" name="id" :value="datos_brand.id">
                             </div>
                         </form>
                     </div>
@@ -174,37 +178,31 @@
                 data(){
                     return {
 
-                        users: <?php echo json_encode($data);?>,
-                        datos_user: {id: "",nombre: "", apellido: "", correo: "", numero: "", creado: "", rol: ""},
+                        brands: <?php echo json_encode($data);?>,
+                        datos_brand: {id: "",nombre: "", descripcion: "", slug: ""},
                         titulo_modal: "",
                         accion: "",
-                        mostrar_añadir_foto: true,
-                        id_user: {id: ""},
                         mostrar_inputs_editar: true,
                         mostrar_inputs_añadir: true,
                         btn_guardar_cambios: "",
                     }
                 },
                 methods:{
-                    editUser(user){
-                        this.titulo_modal = "Editar usuario";
-                        this.datos_user = user;
-                        this.accion = "editarUsuario";
-                        this.mostrar_añadir_foto = false;
+                    editBrand(brand){
+                        this.titulo_modal = "Editar Brand";
+                        this.datos_brand = brand;
+                        this.accion = "editarBrand";
                         this.btn_guardar_cambios = "Cambios";
                         this.mostrar_inputs_editar = true;
                         this.mostrar_inputs_añadir = false;
                     },
-                    agregarUser(){
-                        this.titulo_modal = "Agregar usuario";
-                        this.accion = "nuevoUsuario";   
+                    agregarBrand(){
+                        this.titulo_modal = "Agregar Brand";
+                        this.accion = "nuevaBrand";   
                         this.mostrar_añadir_foto = true;
                         this.btn_guardar_cambios = "";
                         this.mostrar_inputs_editar = false;
                         this.mostrar_inputs_añadir = true;
-                    },
-                    editarFotoUser(user){
-                        this.id_user = user;
                     },
                     alertaEditar(){
                         Swal.fire({
@@ -219,7 +217,7 @@
                         Swal.fire({
                         position: 'center',
                         icon: 'info',
-                        title: 'Usuario Eliminado',
+                        title: 'Etiqueta Eliminada',
                         showConfirmButton: false,
                         timer: 15000
                         })
