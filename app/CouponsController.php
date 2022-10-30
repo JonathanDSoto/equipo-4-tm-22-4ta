9bda313b37f57b1f58630026b2589af682266d1d
Ivan Martin Aviles Moreira <ivanaviles569@gmail.com>
2022-10-29 18:01:50 -0600

    Editar CouponsController

--- app/CouponsController.php
+++ app/CouponsController.php
@@ -17,6 +17,7 @@ if($_SESSION['acceso']=="acceso"){
                     $name = strip_tags($_POST['name']);
                     $code = strip_tags($_POST['code']);
                     $percetage_discount = strip_tags($_POST['percetage_discount']);
+                    $amount_discount = strip_tags($_POST['amount_discount']);
                     $min_amount_required = strip_tags($_POST['min_amount_required']);
                     $min_product_required = strip_tags($_POST['min_product_required']);
                     $start_date = strip_tags($_POST['start_date']);
@@ -26,7 +27,7 @@ if($_SESSION['acceso']=="acceso"){
                     $valid_only_first_purchase = strip_tags($_POST['valid_only_first_purchase']);
                     $status = strip_tags($_POST['status']);
 
-                    $couponsController->newCoupon($name,$code,$percetage_discount,$min_amount_required,$min_product_required,$start_date,$end_date,$max_uses,$count_uses,$valid_only_first_purchase,$status);
+                    $couponsController->newCoupon($name,$code,$percetage_discount,$amount_discount,$min_amount_required,$min_product_required,$start_date,$end_date,$max_uses,$count_uses,$valid_only_first_purchase,$status);
 
                 break;
                 case 'editarCupon':
@@ -35,6 +36,7 @@ if($_SESSION['acceso']=="acceso"){
                     $name = strip_tags($_POST['name']);
                     $code = strip_tags($_POST['code']);
                     $percetage_discount = strip_tags($_POST['percetage_discount']);
+                    $amount_discount = strip_tags($_POST['amount_discount']);
                     $min_amount_required = strip_tags($_POST['min_amount_required']);
                     $min_product_required = strip_tags($_POST['min_product_required']);
                     $start_date = strip_tags($_POST['start_date']);
@@ -45,7 +47,7 @@ if($_SESSION['acceso']=="acceso"){
                     $status = strip_tags($_POST['status']);
                     $id = strip_tags($_POST['id']);
 
-                    $couponsController->editCoupon($name,$code,$percetage_discount,$min_amount_required,$min_product_required,$start_date,$end_date,$max_uses,$count_uses,$valid_only_first_purchase,$status,$id);
+                    $couponsController->editCoupon($name,$code,$percetage_discount,$amount_discount,$min_amount_required,$min_product_required,$start_date,$end_date,$max_uses,$count_uses,$valid_only_first_purchase,$status,$id);
 
                 break;
                 case 'eliminarCupon':
@@ -130,7 +132,7 @@ if($_SESSION['acceso']=="acceso"){
 
         }
     //Nueva Coupon
-        public function newCoupon($name,$code,$percetage_discount,$min_amount_required,$min_product_required,$start_date,$end_date,$max_uses,$count_uses,$valid_only_first_purchase,$status)
+        public function newCoupon($name,$code,$percetage_discount,$amount_discount,$min_amount_required,$min_product_required,$start_date,$end_date,$max_uses,$count_uses,$valid_only_first_purchase,$status)
         {
 
             $curl = curl_init();
@@ -148,6 +150,7 @@ if($_SESSION['acceso']=="acceso"){
                     'name' => $name,
                     'code' => $code,
                     'percentage_discount' => $percetage_discount,
+                    'amount_discount' => $amount_discount,
                     'min_amount_required' => $min_amount_required,
                     'min_product_required' => $min_product_required,
                     'start_date' => $start_date,
@@ -176,7 +179,7 @@ if($_SESSION['acceso']=="acceso"){
         }
 
     //Editar Cupones
-        public function editCoupon($name,$code,$percetage_discount,$min_amount_required,$min_product_required,$start_date,$end_date,$max_uses,$count_uses,$valid_only_first_purchase,$status,$id)
+        public function editCoupon($name,$code,$percetage_discount,$amount_discount,$min_amount_required,$min_product_required,$start_date,$end_date,$max_uses,$count_uses,$valid_only_first_purchase,$status,$id)
         {
 
             $curl = curl_init();
@@ -191,9 +194,10 @@ if($_SESSION['acceso']=="acceso"){
                 CURLOPT_CUSTOMREQUEST => 'PUT',
                 CURLOPT_POSTFIELDS => 
                 'name=cupon%20OP%2020%25'.$name.'&code='.$code.'&percentage_discount='.$percetage_discount.
-                '&min_amount_required='.$min_amount_required.'&min_product_required='.$min_product_required.
-                '&start_date='.$start_date.'&end_date='.$end_date.'&max_uses='.$max_uses.'&count_uses='.$count_uses.
-                '&valid_only_first_purchase='.$valid_only_first_purchase.'&status='.$status.'&id='.$id,
+                '&amount_discount='.$amount_discount.'&min_amount_required='.$min_amount_required.
+                '&min_product_required='.$min_product_required.'&start_date='.$start_date.'&end_date='.$end_date.
+                '&max_uses='.$max_uses.'&count_uses='.$count_uses.'&valid_only_first_purchase='.$valid_only_first_purchase.
+                '&status='.$status.'&id='.$id,
                 CURLOPT_HTTPHEADER => array(
                     'Authorization: Bearer '.$_SESSION['token'],
                     'Content-Type: application/x-www-form-urlencoded'
--- app/config.php
+++ app/config.php
@@ -1,18 +1,12 @@
 <?php 
 	session_start();
-	include_once "AuthController.php";
 
 	
 	if (!isset($_SESSION['global_token'])) {
 		$_SESSION['global_token'] = md5( uniqid( mt_rand(),true ) );
 	}
-	
+
 	if (!defined('BASE_PATH')) {
 		define('BASE_PATH','http://localhost/equipo-4-tm-22-4ta/');
 	}
-	//if($_SESSION['acceso']=="acceso"){
-
-	//}else{
-	//	header("Location:".BASE_PATH."iniciar-sesion");
-	//}
 ?>
